641be51034ac95eb62f3d4db2de0a88fbfc80901
Charles Gavini <>
2022-10-10 17:32:59 +0200

    class acquisition

--- Code_Acquisition_Classes.py
+++ Code_Acquisition_Classes.py
@@ -3,17 +3,13 @@ import matplotlib.pyplot as plt
 from nidaqmx.constants import TerminalConfiguration
 import numpy as np
 import pandas as pd
-
+import sys
 class acquisition_signal_capteur() :
     """
-    frequence = 100000
-    T = 10 #s
+    frequence =
+    T =  #s
     numberOfSamples = T*frequence
-    Liste = np.zeros((numberOfSamples,2)) * np.nan
-    Liste2 = np.zeros((numberOfSamples,2)) * np.nan
-    Liste3 = np.zeros((numberOfSamples,2)) * np.nan
-    Liste4 = np.zeros((numberOfSamples,2)) * np.nan
-    Liste5 = np.zeros((numberOfSamples,2)) * np.nan
+    Liste =
     """
 
 
@@ -24,48 +20,54 @@ class acquisition_signal_capteur() :
         self.n = n
         self.numberOfSamples = T*frequence
         self.Liste= [np.zeros((self.numberOfSamples,2)) * np.nan for i in range(0,n)]
-        
+
 
         # Démarrage Acquisition #
-        
+
         self.task = nidaqmx.Task()
         self.task.ai_channels.add_ai_voltage_chan("Dev1/ai0",terminal_config=TerminalConfiguration.RSE)
         self.task.timing.cfg_samp_clk_timing((frequence), source='', active_edge=nidaqmx.constants.Edge.RISING, sample_mode=nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan=self.numberOfSamples*5)
         self.task.start()
-        
+
         #Acquisition
         for i in range(0,n):
             self.acquisition_signal(i)
-            
+
+        self.to_csv()
+        for i in range(0,n):
+            self.affichage(i)
+
         self.task.stop
         self.task.close()
-        
+
     def acquisition_signal(self,i):
         frequence=self.frequence
         numberOfSamples=self.numberOfSamples
         Liste = self.Liste[i]
         value = self.task.read(numberOfSamples)
-        i=1
-        while i<numberOfSamples :
-            val = value[i]
-            Liste[i,0] = i/frequence
-            Liste[i,1] = val
-            i=i+1
+        k=1
+        while k<numberOfSamples :
+            val = value[k]
+            Liste[k,0] = k/frequence
+            Liste[k,1] = val
+            k=k+1
         print('SIGNAL %d OK' %i)
 
 
 
-    def to_excel(self):
-        
-        for i in range(0,self.n):
-            df=pd.DataFrame(self.Liste[i])
-            df.to_excel('signal%d.xlsx'%i)
-            print('Enregistrement %d OK' %i)
-            
+    def to_csv(self):
+
+        for k in range(0,self.n):
+            self.Liste[0][0] = np.array([0,0]) #valeur initiale
+            df=pd.DataFrame(self.Liste[k])
+            df.to_csv('signal%d.csv'%k)
+            print('Enregistrement %d OK' %k)
+
 
     def affichage(self,i):
         plt.plot(self.Liste[i][:,0],self.Liste[i][:,1])
         plt.show()
 
-
-       
+if __name__ == "__main__":
+    with nidaqmx.Task() as task:
+        a=acquisition_signal_capteur()
